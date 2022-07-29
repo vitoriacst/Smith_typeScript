67e915db145f3f11ff9a12ee5dac5401644e0498
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-29 15:36:17 -0300

    feat: :sparkles: creating the template to list and insert all users

---
+++ src/models/user.model.ts
@@ -0,0 +1,30 @@
+import { ResultSetHeader } from 'mysql2';
+import { Users } from '../interfaces';
+import connection from './connection';
+
+// creating the user and inserting into the table
+const createUser = async (usuários: Users) => {
+  const { username, classe, level, password } = usuários;
+  const [result] = await connection.execute<ResultSetHeader>(
+    `INSERT INTO
+    Trybesmith.Users(username, classe, level , password)
+  VALUES
+    (?, ?, ?);`,
+    [username, classe, level, password],
+  );
+  return result;
+};
+
+// listing all users
+
+const listUsers = async () => {
+  const [users] = await connection.execute(
+    'SELECT * FROM Trybesmith.Users',
+  );
+  return users;
+};
+
+export {
+  createUser,
+  listUsers,
+};
