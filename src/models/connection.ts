036464faa4357799c1ef2bfdd9d5f13c3302a124
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-29 14:52:43 -0300

    feat: :sparkles: Create the connection

---
+++ src/models/connection.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config();
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+});
+
+export default connection;
