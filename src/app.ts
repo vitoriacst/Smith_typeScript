46320598f481301eda604a04f90f1baaa3c73ada
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-29 16:50:03 -0300

    feat: :sparkles: creating the route to list the products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import router from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
+// criando as rotas
+app.use(router);
 
 export default app;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { listProductsController } from '../controllers/products.controller';
 
 const router = Router();
+
+router.post('/products', listProductsController);
 
 export default router;
