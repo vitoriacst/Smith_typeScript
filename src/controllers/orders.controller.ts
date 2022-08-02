dbbdf025d72064aafc436c27b76f272d184bc73a
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-01 21:45:14 -0300

    feat: :sparkles: implementing orders.controller

---
+++ src/controllers/orders.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { Orders } from '../interfaces/orders.interface';
+import listOrdersService from '../services/orders.service';
+
+const listOrdersController = async (req: Request, res: Response) => {
+  const orders = req.body as Orders;
+  const resultado = await listOrdersService(orders);
+  return res.status(200).json(resultado);
+};
+
+export default listOrdersController;
